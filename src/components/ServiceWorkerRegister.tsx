3c94577a5cc3fd23579ee2c57c9940f2c7d27f4b
satwik88 <satwikraj707@gmail.com>
2026-07-24 13:51:49 +0530

    Add debug log to SW register

--- src/components/ServiceWorkerRegister.tsx
+++ src/components/ServiceWorkerRegister.tsx
@@ -3,6 +3,7 @@ import { useEffect } from 'react';
 
 export default function ServiceWorkerRegister() {
   useEffect(() => {
+    console.log('ServiceWorkerRegister mounted');
     if ('serviceWorker' in navigator) {
       window.addEventListener('load', () => {
         navigator.serviceWorker.register('/sw.js').then(registration => {
