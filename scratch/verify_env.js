74320355f1b2924ad49852c5878d43a7e5a422f5
satwik88 <satwikraj707@gmail.com>
2026-08-09 22:44:54 +0530

    security: remove hardcoded test credentials, gitignore scratch/

--- scratch/verify_env.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const dotenv = require('dotenv');
-
-const envConfig = dotenv.parse(fs.readFileSync('.env.local'));
-console.log('--- Env Verification ---');
-console.log('GOOGLE_CLIENT_ID exists:', !!envConfig.GOOGLE_CLIENT_ID);
-console.log('GOOGLE_CLIENT_SECRET exists:', !!envConfig.GOOGLE_CLIENT_SECRET);
-console.log('NEXTAUTH_SECRET exists:', !!envConfig.NEXTAUTH_SECRET);
-console.log('NEXTAUTH_URL:', envConfig.NEXTAUTH_URL);
